7759baa874622e58a9b9836e7a97a5e290bc8530
subha279 <anupambiswas030@gmail.com>
2025-03-01 02:07:16 +0530

    Fix

--- .config/nvim/lua/preferences.lua
+++ .config/nvim/lua/preferences.lua
@@ -8,6 +8,9 @@ opt.expandtab = true
 opt.smartindent = true
 opt.wrap = false
 
+-- Obsidian
+opt.conceallevel = 2
+
 -- Search
 opt.incsearch = true
 opt.ignorecase = true
@@ -43,4 +46,4 @@ opt.clipboard:append("unnamedplus")
 opt.modifiable = true
 opt.encoding = "UTF-8"
 opt.guicursor =
-"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
